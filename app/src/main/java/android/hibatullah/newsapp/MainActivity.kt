466f45564e9e9f2896f78b0ec80bb54067681597
Milzam Hibatullah <hmilzam@gmail.com>
2019-01-02 14:22:07 +0700

    initial commit

---
+++ app/src/main/java/android/hibatullah/newsapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package android.hibatullah.newsapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.hibatullah.newsapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/android/hibatullah/newsapp/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package android.hibatullah.newsapp.ui.main
+
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
